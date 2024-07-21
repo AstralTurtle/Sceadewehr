605b82ae677e6aa95f1557b64bbb0e672e4ffbde
AstralTurtle <jamesacs329@gmail.com>
2024-07-20 20:12:07 -0400

    avoid if elif block for texture setting

--- src/tile.gd
+++ src/tile.gd
@@ -23,27 +23,23 @@ enum ElementType {
 }
 
 func _ready():
+	texture = tile_images[tile_type]
+
 	if tile_type == ElementType.AIR:
-		# texture =
-		texture = tile_images[ElementType.AIR]
+		
 		pass
 	elif tile_type == ElementType.WATER:
-		texture = tile_images[ElementType.WATER]
 		pass
 	elif tile_type == ElementType.FIRE:
-		texture = tile_images[ElementType.FIRE]
 		pass
 	elif tile_type == ElementType.EARTH:
-		texture = tile_images[ElementType.EARTH]
 		pass
 	elif tile_type == ElementType.BODY:
-		texture = tile_images[ElementType.BODY]
 		pass
 	elif tile_type == ElementType.SOUL:
-		texture = tile_images[ElementType.SOUL]
 		pass
 	elif tile_type == ElementType.MERCURY:
-		texture = tile_images[ElementType.MERCURY]
+
 		pass
 	else:
 		texture = load("res://icon.svg")
