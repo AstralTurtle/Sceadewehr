c3cf64e4822b3151713191782a77d368cbb5e8b2
Shadman Syed <shadman3d@gmail.com>
2024-07-21 20:54:51 -0400

    Visualize the alchemy circle

--- src/AlchemistCircle.gd
+++ src/AlchemistCircle.gd
@@ -5,14 +5,24 @@ class_name AlchemistCircle
 var holdings: Array[Tile.ElementType]
 var held_recipe: Recipe = null
 
-var slot1: Button
-var slot2: Button
+@onready var slot1: Button = $Slot1
+@onready var slot2: Button = $Slot2
+@onready var slot1_texture: TextureRect = $Slot1/Slot1Texture
+@onready var slot2_texture: TextureRect = $Slot2/Slot2Texture
+var essence_images = {
+	Tile.ElementType.AIR: load('res://assets/AirEssence.png'),
+	Tile.ElementType.WATER: load('res://assets/WaterEssence.png'),
+	Tile.ElementType.FIRE: load('res://assets/FireEssence.png'),
+	Tile.ElementType.EARTH: load('res://assets/EarthEssence.png'),
+	Tile.ElementType.BODY:  load('res://assets/Body.png'),
+	Tile.ElementType.SOUL: load('res://assets/Soul.png'),
+	Tile.ElementType.MERCURY: load('res://assets/Mercury.png')
+}
 
 signal send_back(item: Tile.ElementType)
 
 func _ready():
-	slot1 = get_node("Slot1")
-	slot2 = get_node("Slot2")
+	pass
 
 func validate_recipe():
 	for r in recipies:
@@ -26,11 +36,13 @@ func clear_items():
 	for item in holdings:
 		emit_signal(send_back.get_name(), item)
 	holdings.clear()
+	visualize()
 
 func clear_item(ind: int):
 	held_recipe = null
 	emit_signal(send_back.get_name(), holdings[ind])
 	holdings.remove_at(ind)
+	visualize()
 
 func add_item(item: Tile.ElementType):
 	if(holdings.size() >= 2):
@@ -38,3 +50,14 @@ func add_item(item: Tile.ElementType):
 	holdings.append(item)
 	if(holdings.size() == 2):
 		validate_recipe()
+	visualize()
+
+func visualize():
+	if holdings.size() > 0:
+		slot1_texture.texture = essence_images.get(holdings[0])
+	else:
+		slot1_texture.texture = null
+	if holdings.size() > 1:
+		slot2_texture.texture = essence_images.get(holdings[1])
+	else:
+		slot2_texture.texture = null
