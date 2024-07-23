31bca5721e7aa9a24535c8dc513ab7ce6a2cc9b9
Shadman Syed <shadman3d@gmail.com>
2024-07-22 23:08:14 -0400

    Add tweening to grid

--- src/GameBoard.gd
+++ src/GameBoard.gd
@@ -1,6 +1,6 @@
 extends GridContainer
 
-@export var swap_anim_time: float = 1
+@export var swap_anim_time: float = 0.5
 var tiles: Array[Array]
 var tile_scene: PackedScene = load("res://Tile.tscn")
 var locked: bool = false
@@ -19,8 +19,11 @@ func on_tile_clicked(pos: Vector2i):
 	print(pos)
 
 	# The tweens
-	# var tween = get_tree().create_tween()
-	# tween.set_parallel(true)
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	# tween.set_trans(Tween.TRANS_ELASTIC) # Too funny
+	tween.set_trans(Tween.TRANS_EXPO)
+	tween.set_ease(Tween.EASE_OUT)
 
 	if selected_tile == null:
 		selected_tile = pos
@@ -30,16 +33,21 @@ func on_tile_clicked(pos: Vector2i):
 		var selected: Tile = tiles[selected_tile.y][selected_tile.x]
 		var new_selection: Tile = tiles[pos.y][pos.x]
 		var selected_index = selected.get_index()
-		# tween.tween_property(selected, "position", pos, swap_anim_time)
-		# tween.tween_property(new_selection, "position", selected_tile, swap_anim_time)
-		move_child(selected, new_selection.get_index())
-		move_child(new_selection, selected_index)
-		selected.set_grid_index(pos)
-		new_selection.set_grid_index(selected_tile)
-		tiles[selected_tile.y][selected_tile.x] = new_selection
-		tiles[pos.y][pos.x] = selected
+		var old_selection_pos = selected.position
+		tween.tween_property(selected, "position", new_selection.position, swap_anim_time)
+		tween.tween_property(new_selection, "position", old_selection_pos, swap_anim_time)
+		locked = true
+		var cleanup = func():
+			move_child(selected, new_selection.get_index())
+			move_child(new_selection, selected_index)
+			selected.set_grid_index(pos)
+			new_selection.set_grid_index(selected_tile)
+			tiles[selected_tile.y][selected_tile.x] = new_selection
+			tiles[pos.y][pos.x] = selected
+			selected_tile = null
+			locked = false
 		selected.set_focused(false)
-		selected_tile = null
+		tween.connect("finished", cleanup)
 	else:
 		print((selected_tile as Vector2).distance_to(pos as Vector2))
 		tiles[selected_tile.y][selected_tile.x].set_focused(false)
