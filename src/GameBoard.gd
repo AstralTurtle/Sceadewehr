2356bd380fed143b92acbb05544f6c7c9363b4e8
Shadman Syed <shadman3d@gmail.com>
2024-07-23 23:05:39 -0400

    Put back the old swapping thingy

--- src/GameBoard.gd
+++ src/GameBoard.gd
@@ -14,48 +14,47 @@ func _ready():
 	pass
 
 func on_tile_clicked(pos: Vector2i):
-	delete_tile(pos)
-	# if locked:
-	# 	return
-	# print(pos)
+	if locked:
+		return
+	print(pos)
 
-	# if selected_tile == null:
-	# 	selected_tile = pos
-	# 	tiles[selected_tile.y][selected_tile.x].set_focused(true)
-	# elif (selected_tile as Vector2).distance_to(pos as Vector2) < 1.5:
-	# 	print((selected_tile as Vector2).distance_to(pos as Vector2))
-	# 	var selected: Tile = tiles[selected_tile.y][selected_tile.x]
-	# 	var new_selection: Tile = tiles[pos.y][pos.x]
-	# 	var selected_index = selected.get_index()
-	# 	var old_selection_pos = selected.position
+	if selected_tile == null:
+		selected_tile = pos
+		tiles[selected_tile.y][selected_tile.x].set_focused(true)
+	elif (selected_tile as Vector2).distance_to(pos as Vector2) < 1.5:
+		print((selected_tile as Vector2).distance_to(pos as Vector2))
+		var selected: Tile = tiles[selected_tile.y][selected_tile.x]
+		var new_selection: Tile = tiles[pos.y][pos.x]
+		var selected_index = selected.get_index()
+		var old_selection_pos = selected.position
 
-	# 	# The tweens
-	# 	var tween = get_tree().create_tween()
-	# 	tween.set_parallel(true)
-	# 	# tween.set_trans(Tween.TRANS_ELASTIC) # Too funny
-	# 	tween.set_trans(Tween.TRANS_EXPO)
-	# 	tween.set_ease(Tween.EASE_OUT)
-	# 	tween.tween_property(selected, "position", new_selection.position, swap_anim_time)
-	# 	tween.tween_property(new_selection, "position", old_selection_pos, swap_anim_time)
-	# 	locked = true
+		# The tweens
+		var tween = get_tree().create_tween()
+		tween.set_parallel(true)
+		# tween.set_trans(Tween.TRANS_ELASTIC) # Too funny
+		tween.set_trans(Tween.TRANS_EXPO)
+		tween.set_ease(Tween.EASE_OUT)
+		tween.tween_property(selected, "position", new_selection.position, swap_anim_time)
+		tween.tween_property(new_selection, "position", old_selection_pos, swap_anim_time)
+		locked = true
 
-	# 	var cleanup = func():
-	# 		move_child(selected, new_selection.get_index())
-	# 		move_child(new_selection, selected_index)
-	# 		selected.set_grid_index(pos)
-	# 		new_selection.set_grid_index(selected_tile)
-	# 		tiles[selected_tile.y][selected_tile.x] = new_selection
-	# 		tiles[pos.y][pos.x] = selected
-	# 		selected_tile = null
-	# 		locked = false
-	# 		clear_board(tiles)
+		var cleanup = func():
+			move_child(selected, new_selection.get_index())
+			move_child(new_selection, selected_index)
+			selected.set_grid_index(pos)
+			new_selection.set_grid_index(selected_tile)
+			tiles[selected_tile.y][selected_tile.x] = new_selection
+			tiles[pos.y][pos.x] = selected
+			selected_tile = null
+			locked = false
+			clear_board(tiles)
 
-	# 	selected.set_focused(false)
-	# 	tween.connect("finished", cleanup)
-	# else:
-	# 	print((selected_tile as Vector2).distance_to(pos as Vector2))
-	# 	tiles[selected_tile.y][selected_tile.x].set_focused(false)
-	# 	selected_tile = null
+		selected.set_focused(false)
+		tween.connect("finished", cleanup)
+	else:
+		print((selected_tile as Vector2).distance_to(pos as Vector2))
+		tiles[selected_tile.y][selected_tile.x].set_focused(false)
+		selected_tile = null
 
 func setup():
 	for i in range(0, 8):
