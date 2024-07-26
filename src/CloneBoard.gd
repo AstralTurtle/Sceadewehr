2b4c46c62f7a51961cca516436d2025a656b1fe8
Shadman Syed <shadman3d@gmail.com>
2024-07-25 23:35:00 -0400

    Add groups for player one and two

--- src/CloneBoard.gd
+++ src/CloneBoard.gd
@@ -1,15 +1,28 @@
 extends Node2D
 class_name CloneBoard
 
+static var player_one_goup: String = "shadow_clones1"
+static var player_two_goup: String = "shadow_clones2"
 @export var perside_creation: int = 5
 @onready var shadow_clone: PackedScene = load("res://shadow_clone.tscn")
 @onready var game_board: GameBoard = get_node("../GameBoard")
 
 func _ready():
-	for i in range(perside_creation*2):
+	# Player 1
+	for i in range(perside_creation):
 		var clone: ShadowClone = shadow_clone.instantiate()
 		clone.global_position = get_random_pos()
 		clone.grid_interval = game_board.size.x/game_board.columns
+		clone.add_to_group(player_one_goup)
+		clone.is_first_players = true
+		add_child(clone)
+	# Player 2
+	for i in range(perside_creation):
+		var clone: ShadowClone = shadow_clone.instantiate()
+		clone.global_position = get_random_pos()
+		clone.grid_interval = game_board.size.x/game_board.columns
+		clone.add_to_group(player_two_goup)
+		clone.is_first_players = false
 		add_child(clone)
 
 
--- src/ShadowClone.gd
+++ src/ShadowClone.gd
@@ -3,6 +3,7 @@ class_name ShadowClone
 
 static var group_name: String = "shadow_clones"
 var grid_interval: float
+var is_first_players: bool
 
 func move_forward():
 	position.y += grid_interval
