4b5da037ff7139af7c3e7e8ad3e6bb60ad25cd52
Shadman Syed <shadman3d@gmail.com>
2024-07-29 19:01:09 -0400

    you can place items

--- src/HeldItem.gd
+++ src/HeldItem.gd
@@ -1,14 +1,9 @@
 extends TextureButton
 class_name HeldItem
 
-var placer: PackedScene
 var item_scene: PackedScene
 
 signal item_triggered(item_scene: PackedScene, pos: Vector2)
-
-func spawn_placer():
-    var p: ItemPlacer = placer.instantiate()
-    p.item_placed.connect(trigger_item)
 
 func trigger_item(pos: Vector2):
     item_triggered.emit(item_scene, pos)
