c09e57f467d887b026c7df7580c330415d1342f7
AstralTurtle <jamesacs329@gmail.com>
2024-07-29 20:01:14 -0400

    flare item

--- Flare.gd
+++ Flare.gd
@@ -5,7 +5,21 @@ extends BaseItem
 func _ready():
 	pass # Replace with function body.
 
-
+func item_action():
+	print('item_action')
+	var particles: GPUParticles2D = get_node('GPUParticles2D')
+	var area: Area2D = get_node("Area2D")
+	particles.one_shot = true
+	particles.emitting = true
+	await particles.finished
+	var areas = area.get_overlapping_areas()
+	
+	print(areas)
+	for a in areas:
+		var parent = a.get_parent()
+		if parent is ShadowClone:
+			(parent as ShadowClone).destroy()
+	queue_free()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
