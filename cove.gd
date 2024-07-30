84220ea770479fd164ebe04b88ad059ade4a25a6
AstralTurtle <jamesacs329@gmail.com>
2024-07-29 21:41:30 -0400

    cove fixed????

--- cove.gd
+++ cove.gd
@@ -9,6 +9,7 @@ func item_action():
 	var world: MainWorld = get_tree().root.get_node('MainWorld')
 	world.get_player(player).shielded = true
 	queue_free()
+	should_be_deleted = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
